7850fe2b17f1e7bc594d5aebb1c847fdc985287f
Juan Alejandro Villodres <jvillodres@unal.edu.co>
2025-08-08 13:08:57 -0500

    Initial project version

---
+++ main.c
@@ -0,0 +1,14 @@
+// Stacks
+#include <stdio.h>
+
+int main() {
+    /* stack = LIFO data structure. Last-In First-Out
+               stores objects into a sort of "vertical tower"
+               push() to add to the top
+               pop() to remove from the top
+    */
+
+    
+
+    return 0;
+}
